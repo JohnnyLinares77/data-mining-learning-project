6e718880b2b68cb7c48afa83a71583f285d47145
JohnnyLinares77 <j.linares@asesoriamadeeasy.com>
2025-11-01 12:35:32 -0500

    Tesis-NodoViz-3

--- R/mod_m4_ui.R
+++ R/mod_m4_ui.R
@@ -307,9 +307,8 @@ mod_m4_ui <- function(id){
       )
     )
   )
-    ) # cierre del div main_panel
-  ) # cierre del shinyjs::hidden
- ) # cierre del column derecho
-) # cierre del fluidRow
-) # cierre del fluidPage
+          ) # cierre del div main_panel
+        ) # cierre del shinyjs::hidden
+    ) # cierre del fluidRow
+  ) # cierre del fluidPage
 }
